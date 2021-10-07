d599eb827503b4992e675bdec12cda1a3e835b91
zdurr22 <zdurr22@gmail.com>
2021-10-07 12:52:20 -0400

    commit tree b279c7b2067ffec3c11ad8ec7133767e1cedabb4

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+const mapToNegativize = function(sourceArray) {
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArr.push(-1 * sourceArray[i])
+    }
+    return newArr;
+}
+
+const mapToNoChange = function (sourceArray) {
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArr.push(sourceArray[i])
+    }
+    return newArr;
+}
+
+const mapToDouble = function(sourceArray) {
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArr.push(2 * sourceArray[i])
+    }
+    return newArr;
+}
+
+const mapToSquare = function(sourceArray) {
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArr.push(sourceArray[i] ** 2)
+    }
+    return newArr;
+}
+
+function reduceToTotal(src, starting=0) {
+    let total = starting
+    for (let i = 0; i < src.length; i++) {
+        total = total + src[i]
+    }
+    return total;
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++) {
+        if (!src[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++) {
+        if (src[i]) return true
+    }
+    return false
+}
